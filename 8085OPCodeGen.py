d2a5af75f2ad7e6c88c55184f1429dfcb635f03d
Gobi Elankovan <gobigs1999@gmail.com>
2018-09-11 21:20:41 +0530

    DB Insertion

--- 8085OPCodeGen.py
+++ 8085OPCodeGen.py
@@ -1,5 +1,23 @@
+import sqlite3
+
 file1 = open("/home/gobi/Desktop/8085OPCodeGenerator/db.txt","r+")
 file2 = open("/home/gobi/Desktop/8085OPCodeGenerator/db1.txt","w+")
 
-for line in file1:
-    new = line.split(" ")
+database = sqlite3.connect("OPCodes.db")
+db = database.cursor()
+
+# for line in file1:
+#     temp = []
+#     words = line.split(" ")
+#     temp.append(words[0])
+#     temp.append(" ".join(words[1:-2]))
+#     temp.append(words[-2])
+
+    # db.execute("Insert into OPCodes (Size,Instruction,OPCode) VALUES (?,?,?)",temp)
+
+for row in db.execute("SELECT * FROM OPCodes"):
+    print(row)
+
+database.commit()    
+file1.close()
+file2.close()
