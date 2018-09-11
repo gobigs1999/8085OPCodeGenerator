82c140753d62aff4b2195abc263fcaa75c18652a
Gobi Elankovan <gobigs1999@gmail.com>
2018-09-11 23:13:13 +0530

    Code Stage 1

--- 8085OPCodeGen.py
+++ 8085OPCodeGen.py
@@ -1,21 +1,15 @@
 import sqlite3
 
-file1 = open("/home/gobi/Desktop/8085OPCodeGenerator/db.txt","r+")
-file2 = open("/home/gobi/Desktop/8085OPCodeGenerator/db1.txt","w+")
+file1 = open("/home/gobi/Desktop/8085OPCodeGenerator/input.txt","r+")
+file2 = open("/home/gobi/Desktop/8085OPCodeGenerator/output.txt","w+")
 
 database = sqlite3.connect("OPCodes.db")
 db = database.cursor()
 
-# for line in file1:
-#     temp = []
-#     words = line.split(" ")
-#     temp.append(words[0])
-#     temp.append(" ".join(words[1:-2]))
-#     temp.append(words[-2])
-
-    # db.execute("Insert into OPCodes (Size,Instruction,OPCode) VALUES (?,?,?)",temp)
-
-for row in db.execute("SELECT * FROM OPCodes"):
+for line in file1:
+    line=line.split()
+    print(line[0])
+    row = db.execute("SELECT * FROM OPCodes WHERE Instruction LIKE '?%'",line[0])
     print(row)
 
 database.commit()    
