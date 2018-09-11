75f05b64dd6be7307f46d24020742ecc07bf4c2f
Gobi Elankovan <gobigs1999@gmail.com>
2018-09-11 17:15:47 +0530

    Refining OP Codes

--- 8085OPCodeGen.py
+++ 8085OPCodeGen.py
@@ -1,6 +1,5 @@
-# file = open("/home/gobi/Desktop/8085OPCodeGenerator/input.txt","r")
+file1 = open("/home/gobi/Desktop/8085OPCodeGenerator/db.txt","r+")
+file2 = open("/home/gobi/Desktop/8085OPCodeGenerator/db1.txt","w+")
 
-# for line in file:
-#     print(line)
-
-file = 
+for line in file1:
+    new = line.split(" ")
