0cdc876a635bf543412a556f3bf1afd409d29904
Gobi Elankovan <gobigs1999@gmail.com>
2018-09-11 12:50:05 +0530

    Sample Commit

--- 8085OPCodeGen.py
+++ 8085OPCodeGen.py
@@ -1,11 +1,6 @@
-# code = []
-# while True:
-#     temp = input()
-#     code.append(temp)
-#     if temp =="HLT":
-#         break
+# file = open("/home/gobi/Desktop/8085OPCodeGenerator/input.txt","r")
 
-file = open("/home/gobi/Desktop/8085OPCodeGenerator/input.txt","r")
+# for line in file:
+#     print(line)
 
-for line in file:
-    print(line)
+file = 
